3c3203d10685cb0b61bda46ea64a54277e5a584d
hac <70646122+hac01@users.noreply.github.com>
2022-02-01 00:36:07 +0530

    Add files via upload

---
+++ ch1/loop.c
@@ -0,0 +1,9 @@
+// while loop
+#include <stdio.h>
+int main(){
+    int a=10;
+    while (a<20){
+        printf("Your system is hacked check your task bar . i WILL STEAL YOU GF");
+        
+    }
+}
